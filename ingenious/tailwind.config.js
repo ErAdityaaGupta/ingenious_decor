896ba12c6e0545fb97bbd7d9a7cc5e215a793e66
ErAdityaaGupta <159624433+ErAdityaaGupta@users.noreply.github.com>
2025-02-11 18:30:17 +0530

    adding small screen responsiveness

--- ingenious/tailwind.config.js
+++ ingenious/tailwind.config.js
@@ -1,28 +1,44 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // This includes all your component files
-  ],
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // This includes all your component files
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         lightcream: "#F5F3E7",
+//       },
+
+//       width: {
+//         "custom-logo": "300px",
+//         "custom-logo2": "550px"
+//       },
+
+//       height: {
+//         background: "800px",
+//         images: "400px",
+//         portfolio: "600px",
+//       },
+//       fontFamily: {
+//         montserrat: ["Montserrat", "sans-serif"],
+//         poppins: ["Poppins", "sans-serif"],
+//         playfair: ["Playfair Display", "serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+
+export default {
   theme: {
     extend: {
-      colors: {
-        lightcream: "#F5F3E7",
-      },
-
-      width: {
-        "custom-logo": "300px",
-        "custom-logo2": "550px"
-      },
-
-      height: {
-        background: "800px",
-        images: "400px",
-        portfolio: "600px",
-      },
-      fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-        playfair: ["Playfair Display", "serif"],
+      screens: {
+        xs: '375px', // Small mobile screens
+        sm: '640px', // Default small screen
+        md: '768px', // Tablets
+        lg: '1024px', // Laptops
+        xl: '1280px', // Desktops
       },
     },
   },
