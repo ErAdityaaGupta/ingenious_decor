70ce78c05dd54bca560018d82ad381f1b77ae6cf
ErAdityaaGupta <159624433+ErAdityaaGupta@users.noreply.github.com>
2025-02-11 18:32:35 +0530

    Create Services.js

---
+++ ingenious/src/components/Services.js
@@ -0,0 +1,9 @@
+const Services = () => (
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+    <div className="p-4 bg-gray-100 rounded-lg">Service 1</div>
+    <div className="p-4 bg-gray-100 rounded-lg">Service 2</div>
+    <div className="p-4 bg-gray-100 rounded-lg">Service 3</div>
+  </div>
+);
+
+export default Services;
